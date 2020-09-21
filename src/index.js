f40e99aa91a021110cacc7cf4883abbbed37bb22
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-21 23:38:53 +0500

    templets litrels

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const myName = "Talha Bajwa";
+const fName = "Talha Bajwa";
+const lName = "Saab";
 const luckyNum = 10;
 
 ReactDom.render(
   <>
-    <h1>My name is {myName}.</h1>
-    <h2>My lucky number is {luckyNum}. </h2>
+    <h1>{`My name is ${fName} ${lName}`}.</h1>
+    <h2>{`My lucky number is ${luckyNum}`}. </h2>
     <h1>Netflix Series</h1>
     <ol>
       <li>Ertugral ghazi</li>
